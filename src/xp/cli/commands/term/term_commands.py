a102b2218717c32da0a5c40e1a8bc27443cdec4f
lduchosal <luc+github@2113.ch>
2025-11-16 23:44:42 +0100

    chore: term / cleanup

--- src/xp/cli/commands/term/term_commands.py
+++ src/xp/cli/commands/term/term_commands.py
@@ -21,7 +21,7 @@ def protocol_monitor(ctx: Context) -> None:
         \b
         xp term protocol
     """
-    from xp.term.app import ProtocolMonitorApp
+    from xp.term.protocol import ProtocolMonitorApp
 
     # Resolve ServiceContainer from context
     container = ctx.obj.get("container").get_container()
