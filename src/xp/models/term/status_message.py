18b086292a4edff20e7c8e10fd7df80e3dd4a7bb
lduchosal <luc+github@2113.ch>
2025-11-17 08:50:40 +0100

    refactor: term / protocol / cleanup

--- src/xp/models/term/status_message.py
+++ src/xp/models/term/status_message.py
@@ -2,3 +2,15 @@
 
 from textual.message import Message
 
+
+class StatusMessageChanged(Message):
+    """Message posted when status message changes."""
+
+    def __init__(self, message: str) -> None:
+        """Initialize the message.
+
+        Args:
+            message: The status message to display.
+        """
+        super().__init__()
+        self.message = message
